8161d34e108f13fae5c38a9a04ebd5c237e10a5e
Bappa Kundu <bappakn22@gmail.com>
2025-06-24 20:23:02 +0530

    i have modified some information

--- Resource_Nested_Map/infra_group/provider.tf
+++ Resource_Nested_Map/infra_group/provider.tf
@@ -6,17 +6,6 @@ terraform {
     }
   }
 
-  backend "azurerm" {
-    resource_group_name  = "bappa_rg"
-    storage_account_name = "bappastrg"
-    container_name       = "datacont"
-    key                  = "prod.terraform.tfstate"
-  }
 
 }
 
-provider "azurerm" {
-  features {}
-  subscription_id = "ba578ce6-e042-4ed5-9e0b-99d4cc681a62"
-  # Configuration options
-}
